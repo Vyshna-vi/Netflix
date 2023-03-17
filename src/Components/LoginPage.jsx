e9d312f095c07afd2c2bff46696680a89cf024d0
Vyshnavi Santhosh <vyshnaviponnuzz0001@gmail.com>
2023-03-17 09:18:07 +0530

    started signin page

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -6,24 +6,29 @@ function LoginPage() {
     const navigate = useNavigate()
 
     return (
-        <div className='inner'>
-            <img src="../logo/logoimg.com" alt="logo" />
-            <div className='inner1'>
-                <h1>Sign In</h1>
-                <input type="text" placeholder='Email or phone number' className='inp' /><br /><br />
-                <input type="text" placeholder='Password' className='inp' />
-            </div>
-            <div className='inner2'>
-                <button className='btn'>Sign In</button>
-            </div>
-            <div className='inner3'>
-                <input type="checkbox" id="" name="rem" />
-                <label for="rem" className='rem'> Rememeber Me  </label>
-                <a href="" className='need'>Need help?</a>
-            </div>
-            <div className='inner4'>
-                <p className='para'>New to Netflix?<a href="" className='link1'>Sign up now</a></p>
-                <p className='para paraa'>This page is protected by Google reCAPTCHA to ensure you're not a bot. <a href="" className='link2'>Learn more.</a></p>
+        <div className='body1'>
+            <div className='main'>
+                <img src="/logo/logoimg.png" alt="logo" className='logo' width="200px" height="112px" />
+                <div className='inner'>
+                    <div className='inner1'>
+                        <p className='h1'>Sign In</p>
+                        <input type="text" placeholder='Email or phone number' className='inp' name='email' />
+                        <br /><br />
+                        <input type="text" placeholder='Password' className='inp' name='pass' />
+                    </div>
+                    <div className='inner2'>
+                        <button className='btn'>Sign In</button>
+                    </div>
+                    <div className='inner3'>
+                        <input type="checkbox" id="" name="rem" className='checkbox' />
+                        <label for="rem" className='rem'> Rememeber me  </label>
+                        <a href="" className='need'>Need help?</a>
+                    </div>
+                    <div className='inner4'>
+                        <p className='para'>New to Netflix?<a href="" className='link1'>Sign up now</a></p>
+                        <p className='para paraa'>This page is protected by Google reCAPTCHA to ensure you're not a bot. <a href="" className='link2'>Learn more.</a></p>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import '../Signin.css'
 
 function SignUp() {
     const navigate = useNavigate()
@@ -8,11 +9,30 @@ function SignUp() {
         navigate("/login")
     }
     return (
-        <div>
-            <h1>
-                Sign Up
-            </h1>
-            <button onClick={submit}>Login</button>
+        <div className='body2'>
+            <div className='nav'>
+                <img src="/logo/logoimg.png" alt="logo" className='logo1' width="200px" height="112px" />
+                <div className='lan'>
+                    <select name="" id="">
+                        <option value="">English</option>
+                        <option value="">Hindi</option>
+                    </select>
+                    <button>Sign In</button>
+                </div>
+                <div className='main2'>
+                    <div className='div1'>
+                        <p className='para4'>
+                            Unlimited movies, TV shows and more.
+                        </p>
+                        <p className='para1'>Watch anywhere. Cancel anytime.</p>
+                        <p className='para2'>Ready to watch? Enter your email to create or restart your membership.</p>
+                    </div>
+                    <div className='div2'>
+                        <input type="text" name="" id="" placeholder='Email address' className='inpt' />
+                        <button className='btn1'>Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
